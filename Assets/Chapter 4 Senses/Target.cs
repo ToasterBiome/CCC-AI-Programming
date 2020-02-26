b87a1528ee9e6f4a68e00a1907000e8bcdfaa298
Alex Dimitrijevic <baiomurang@gmail.com>
2020-02-25 20:22:55 -0600

    Chapter 4

---
+++ Assets/Chapter 4 Senses/Target.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Target : MonoBehaviour
+{
+    void Update()
+    {
+        int button = 0;
+        //Get the point of the hit position when the mouse is being clicked
+        if (Input.GetMouseButtonDown(button))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                Vector3 targetPosition = hitInfo.point;
+                transform.position = targetPosition;
+            }
+        }
+    }
+}
